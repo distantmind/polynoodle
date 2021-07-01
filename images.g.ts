67e148c25ff700de669af6a22b56e71a429547c6
distantmind <77812885+distantmind@users.noreply.github.com>
2021-07-01 22:39:11 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,102 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case ")lLaz}[Y=-i%xqG2$dq`":
+            case "Chopsticks":return img`
+d d . . . . d d 
+d d . . . . d d 
+d d . . . . d d 
+d d . . . . d d 
+d d . . . . d d 
+2 2 . . . . 2 2 
+d d . . . . d d 
+2 2 . . . . 2 2 
+`;
+            case "image2":
+            case "chopsticks":return img`
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . 2 . . . 2 . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . d . . . d . . . . . . 
+. . . . . 2 . . . 2 . . . . . . 
+. . . . . 2 . . . 2 . . . . . . 
+`;
+            case "image1":
+            case "noodle bowl":return img`
+..eee...................
+eeedde..................
+edeedde.................
+eddeedde................
+.eddeedd4444444.........
+..eddee4555445444.......
+...edde44444445544444...
+...4444455455544444444..
+..455555544444444555554.
+.4544444444555555444444.
+ffffffffffffffffffffffff
+f199111991111111111111bf
+f911919119111191111119bf
+f9991191191111111111bbbf
+f91111911919111119bbbbbf
+ffffffffffffffffffffffff
+.f11111111111111111bbbf.
+.f111ff1111111111ffbbbf.
+..f1ff1f1111111bf1ffbf..
+..f1ffff111111bbffffbf..
+...f1ff11111bbbbbffbf...
+....f33111bbbbbbb33f....
+....ffbbbbbbbbbbbbff....
+.....ffffffffffffff.....
+`;
+            case "image4":
+            case "noodles":return img`
+..eee...................
+eeedde..................
+edeedde.................
+eddeedde................
+.eddeedd4444444.........
+..eddee4555445444.......
+...edde44444445544444...
+...4444455455544444444..
+..455555544444444555554.
+.4544444444555555444444.
+..4444445555554444444454
+..455555444444445555554.
+...4444444455545544444..
+....44444554444444......
+........4445445554......
+..........4444444.......
+........................
+........................
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
