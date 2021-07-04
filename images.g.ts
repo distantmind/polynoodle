0079ab52743d9468346cd1d03e044597de82fdc1
distantmind <77812885+distantmind@users.noreply.github.com>
2021-07-04 16:08:09 +1000

    stars and steam

--- images.g.ts
+++ images.g.ts
@@ -33,33 +33,6 @@ d d . . . . d d
 . . . . . 2 . . . 2 . . . . . . 
 . . . . . 2 . . . 2 . . . . . . 
 `;
-            case "image1":
-            case "noodle bowl":return img`
-..eee...................
-eeedde..................
-edeedde.................
-eddeedde................
-.eddeedd4444444.........
-..eddee4555445444.......
-...edde44444445544444...
-...4444455455544444444..
-..455555544444444555554.
-.4544444444555555444444.
-ffffffffffffffffffffffff
-f199111991111111111111bf
-f911919119111191111119bf
-f9991191191111111111bbbf
-f91111911919111119bbbbbf
-ffffffffffffffffffffffff
-.f11111111111111111bbbf.
-.f111ff1111111111ffbbbf.
-..f1ff1f1111111bf1ffbf..
-..f1ffff111111bbffffbf..
-...f1ff11111bbbbbffbf...
-....f33111bbbbbbb33f....
-....ffbbbbbbbbbbbbff....
-.....ffffffffffffff.....
-`;
             case "image4":
             case "noodles":return img`
 ..eee...................
@@ -86,6 +59,60 @@ eddeedde................
 ........................
 ........................
 ........................
+`;
+            case "image3":
+            case "noodle bowl0":return img`
+11eee1111111111111111111
+eeedde111111111111111111
+edeedde11111111111111111
+eddeedde1111111111111111
+1eddeedd4444444111111111
+11eddee45554454441111111
+111edde44444445544444111
+111444445545554444444411
+114555555444444445555541
+145444444445555554444441
+ffffffffffffffffffffffff
+f199111991111111111111bf
+f911919119111191111119bf
+f9991191191111111111bbbf
+f91111911919111119bbbbbf
+ffffffffffffffffffffffff
+1f11111111111111111bbbf1
+1f111ff1111111111ffbbbf1
+11f1ff1f1111111bf1ffbf11
+11f1ffff111111bbffffbf11
+111f1ff11111bbbbbffbf111
+1111f33111bbbbbbb33f1111
+1111ffbbbbbbbbbbbbff1111
+11111ffffffffffffff11111
+`;
+            case "image1":
+            case "noodle bowl":return img`
+..eee...1....1...1......
+eeedde...1..1....1......
+edeedde..1...1....1.....
+eddeedde1....1...1......
+.eddeedd4444444...1.....
+..eddee4555445444.1.....
+...edde44444445544444...
+...4444455455544444444..
+..455555544444444555554.
+.4544444444555555444444.
+ffffffffffffffffffffffff
+f199111991111111111111bf
+f911919119111191111119bf
+f9991191191111111111bbbf
+f91111911919111119bbbbbf
+ffffffffffffffffffffffff
+.f11111111111111111bbbf.
+.f111ff1111111111ffbbbf.
+..f1ff1f1111111bf1ffbf..
+..f1ffff111111bbffffbf..
+...f1ff11111bbbbbffbf...
+....f33111bbbbbbb33f....
+....ffbbbbbbbbbbbbff....
+.....ffffffffffffff.....
 `;
         }
         return null;
